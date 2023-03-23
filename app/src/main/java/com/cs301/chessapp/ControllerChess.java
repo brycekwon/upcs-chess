cdf82454500412f088fb15e79512d9ef26bed494
camero1993 <grahamm26@up.edu>
2023-03-23 11:07:50 -0700

    controller implements onTouch

--- app/src/main/java/com/cs301/chessapp/ControllerChess.java
+++ app/src/main/java/com/cs301/chessapp/ControllerChess.java
@@ -3,12 +3,14 @@ package com.cs301.chessapp;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class ControllerChess {
+public class ControllerChess implements View.OnTouchListener {
 
     public ControllerChess() {
 
     }
+
+    @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        return true;
+        return false;
     }
 }
