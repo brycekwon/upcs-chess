5eeaba6e1a8a3e3c2a1a266e0ed25716bf0232a1
Marshy <zhangm24@up.edu>
2023-03-23 11:00:30 -0700

    Model Class constructor, setters, getters

--- app/src/main/java/com/cs301/chessapp/ModelChess.java
+++ app/src/main/java/com/cs301/chessapp/ModelChess.java
@@ -1,7 +1,30 @@
 package com.cs301.chessapp;
 
+
+import com.cs301.chessapp.gamestate.chessboard.Board;
+import com.cs301.chessapp.gamestate.utilities.ChessTimer;
+
 public class ModelChess {
     private String player1Name;
     private String player2Name;
+    private ChessTimer timer;
+    private Board board;
+
+    public ModelChess(){
+        this.player1Name = "Player UNO";
+        this.player2Name ="Player Dos";
+        this.timer = new ChessTimer();
+        this.board = new Board();
+    }
+
+    public void setPlayer1Name(String name){
+        this.player1Name = name;
+    }
+    public void setPlayer2Name(String name){
+        this.player2Name = name;
+    }
+    public String getPlayer1Name(){return this.player1Name;}
+    public String getPlayer2Name(){return this.player2Name;}
+
 
 }
