93dceb6cc887e6ffa3ec32f6815ad3017966129d
camero1993 <grahamm26@up.edu>
2023-03-23 10:47:38 -0700

    controller

---
+++ app/src/main/java/com/example/chessapp/ControllerChess.java
@@ -0,0 +1,14 @@
+package com.example.chessapp;
+
+import android.view.MotionEvent;
+import android.view.View;
+
+public class ControllerChess {
+
+    public ControllerChess() {
+
+    }
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return true;
+    }
+}
