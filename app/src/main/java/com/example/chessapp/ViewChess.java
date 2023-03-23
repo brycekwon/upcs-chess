6f9359de36c2d9e87372340ca9fbea7bb82346a3
Christopher Yee <yeec26@up.edu>
2023-03-23 10:32:22 -0700

    view for chess app pt3

--- app/src/main/java/com/example/chessapp/ViewChess.java
+++ app/src/main/java/com/example/chessapp/ViewChess.java
@@ -1,9 +1,7 @@
 package com.example.chessapp;
 
 import android.content.Context;
-import android.view.MotionEvent;
 import android.view.SurfaceView;
-import android.view.View;
 
 public class ViewChess extends SurfaceView {
 
