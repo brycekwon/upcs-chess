13ff8f86da02f6bf425f7dcfafdd6229c7c86826
Bryce Kwon <kwon25@up.edu>
2023-04-28 16:59:31 -0700

    Add comments and minor fixes for Rook.java
    - Updated documentation and comments
    - Added newPiece variable to lower calls to getPiece() method
    - Removed unnecessary checks for getChecks() method

--- app/src/main/java/com/cs301/chessapp/gamestate/pieces/Rook.java
+++ app/src/main/java/com/cs301/chessapp/gamestate/pieces/Rook.java
@@ -3,14 +3,17 @@ package com.cs301.chessapp.gamestate.pieces;
 import java.util.ArrayList;
 
 import com.cs301.chessapp.gameframework.players.GamePlayer;
+
 import com.cs301.chessapp.gamestate.ChessGameState;
 import com.cs301.chessapp.gamestate.chessboard.ChessMove;
 
 /**
  * Rook class
- *
- * This class represents a rook piece in a game of chess. The rook can move any
- * number of squares horizontally or vertically. It is worth 5 points.
+ * <p>
+ * This class represents a rook in the game of chess. The rook can move any
+ * number of squares horizontally or vertically. It cannot jump over other
+ * pieces. It captures pieces by moving to their square. It is a subclass of
+ * Piece.
  *
  * @author Bryce Kwon
  * @author Christopher Yee
@@ -21,32 +24,36 @@ import com.cs301.chessapp.gamestate.chessboard.ChessMove;
 public class Rook extends Piece {
 
     /**
-     * Rook constructor
+     * Rook default constructor
+     * <p>
+     * This constructor initializes a rook assigned to a player.
      *
-     * This constructor initializes a rook with a player.
-     *
-     * @param player        the player the piece belongs to
+     * @param playerId      player assigned to this piece
      */
-    public Rook(int player) {
-        super(player, "Rook");
+    public Rook(int playerId) {
+        // invoke superclass constructor
+        super(playerId, "Rook");
     }
 
     /**
      * getMoves
+     * <p>
+     * This method calculates all valid moves for the rook at its current
+     * position on the chessboard.
      *
-     * This method returns all valid moves for the king.
-     *
-     * @param gamestate     the current gamestate
-     * @param player        the player making the move
-     * @return              a list of valid moves
+     * @param gamestate     current state of the game
+     * @param player        player assigned to this piece
+     * @return              valid moves for the rook
      */
     @Override
     public ArrayList<ChessMove> getMoves(ChessGameState gamestate, GamePlayer player) {
+        // initialize an empty list of valid moves
         ArrayList<ChessMove> validMoves = new ArrayList<>();
 
         // these variables specify the new piece location
         int newRow;
         int newCol;
+        Piece newPiece;
 
         // these variables keep track of blocked paths
         boolean blocked_1 = false;
@@ -54,23 +61,25 @@ public class Rook extends Piece {
         boolean blocked_3 = false;
         boolean blocked_4 = false;
 
+        // traverse the board in all four directions
         for (int i = 1; i < 8; i++) {
             if (!blocked_1 && hasValidBounds(_row, _col + i)) {
                 newRow = _row;
                 newCol = _col + i;
+                newPiece = gamestate.getPiece(newRow, newCol);
 
-                // checked tile is empty
-                if (gamestate.getPiece(newRow, newCol) == null) {
+                // current tile is empty (valid move)
+                if (newPiece == null) {
                     validMoves.add(new ChessMove(player, _row, _col, newRow, newCol));
                 }
 
-                // checked tile has a capturable piece
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() != _playerId) {
+                // current tile has a capturable piece (last valid move)
+                else if (newPiece.getPlayerId() != _playerId) {
                     validMoves.add(new ChessMove(player, _row, _col, newRow, newCol));
                     blocked_1 = true;
                 }
 
-                // checked tile has one of own pieces
+                // current tile has one of own pieces (invalid move)
                 else {
                     blocked_1 = true;
                 }
@@ -79,19 +88,20 @@ public class Rook extends Piece {
             if (!blocked_2 && hasValidBounds(_row, _col - i)) {
                 newRow = _row;
                 newCol = _col - i;
+                newPiece = gamestate.getPiece(newRow, newCol);
 
-                // checked tile is empty
-                if (gamestate.getPiece(newRow, newCol) == null) {
+                // current tile is empty (valid move)
+                if (newPiece == null) {
                     validMoves.add(new ChessMove(player, _row, _col, newRow, newCol));
                 }
 
-                // checked tile has a capturable piece
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() != _playerId) {
+                // current tile has a capturable piece (last valid move)
+                else if (newPiece.getPlayerId() != _playerId) {
                     validMoves.add(new ChessMove(player, _row, _col, newRow, newCol));
                     blocked_2 = true;
                 }
 
-                // checked tile has one of own pieces
+                // current tile has one of own pieces (invalid move)
                 else {
                     blocked_2 = true;
                 }
@@ -100,19 +110,20 @@ public class Rook extends Piece {
             if (!blocked_3 && hasValidBounds(_row + i, _col)) {
                 newRow = _row + i;
                 newCol = _col;
+                newPiece = gamestate.getPiece(newRow, newCol);
 
-                // checked tile is empty
-                if (gamestate.getPiece(newRow, newCol) == null) {
+                // current tile is empty (valid move)
+                if (newPiece == null) {
                     validMoves.add(new ChessMove(player, _row, _col, newRow, newCol));
                 }
 
-                // checked tile has a capturable piece
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() != _playerId) {
+                // current tile has a capturable piece (last valid move)
+                else if (newPiece.getPlayerId() != _playerId) {
                     validMoves.add(new ChessMove(player, _row, _col, newRow, newCol));
                     blocked_3 = true;
                 }
 
-                // checked tile has one of own pieces
+                // current tile has one of own pieces (invalid move)
                 else {
                     blocked_3 = true;
                 }
@@ -121,34 +132,44 @@ public class Rook extends Piece {
             if (!blocked_4 && hasValidBounds(_row - i, _col)) {
                 newRow = _row - i;
                 newCol = _col;
+                newPiece = gamestate.getPiece(newRow, newCol);
 
-                // checked tile is empty
-                if (gamestate.getPiece(newRow, newCol) == null) {
+                // current tile is empty (valid move)
+                if (newPiece == null) {
                     validMoves.add(new ChessMove(player, _row, _col, newRow, newCol));
                 }
 
-                // checked tile has a capturable piece
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() != _playerId) {
+                // current tile has a capturable piece (last valid move)
+                else if (newPiece.getPlayerId() != _playerId) {
                     validMoves.add(new ChessMove(player, _row, _col, newRow, newCol));
                     blocked_4 = true;
                 }
 
-                // checked tile has one of own pieces
+                // current tile has one of own pieces (invalid move)
                 else {
                     blocked_4 = true;
                 }
             }
         }
 
+        // return the list of valid moves
         return validMoves;
     }
 
+    /**
+     * getChecks
+     * <p>
+     * This methods calculates all checkable moves for the rook at its
+     * current position on the chessboard. This method is used to determine
+     * whether the rook is putting the opposing player's king in check.
+     *
+     * @param gamestate     current state of the game
+     * @return              checkable moves for the rook
+     */
+    @Override
     public ArrayList<ChessMove> getChecks(ChessGameState gamestate) {
+        // initialize the list of valid moves
         ArrayList<ChessMove> validMoves = new ArrayList<>();
-
-        // these variables specify the new piece location
-        int newRow;
-        int newCol;
 
         // these variables keep track of blocked paths
         boolean blocked_1 = false;
@@ -158,105 +179,29 @@ public class Rook extends Piece {
 
         for (int i = 1; i < 8; i++) {
             if (!blocked_1 && hasValidBounds(_row, _col + i)) {
-                newRow = _row;
-                newCol = _col + i;
-
-                // checked tile is empty
-                if (gamestate.getPiece(newRow, newCol) == null) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                }
-
-                // checked tile has a capturable piece
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() != _playerId) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                    blocked_1 = true;
-                }
-
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() == _playerId) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                    blocked_1 = true;
-                }
-
-                // checked tile has one of own pieces
-                else {
+                validMoves.add(new ChessMove(null, _row, _col, _row, _col + i));
+                if (gamestate.getPiece(_row, _col + i) != null) {
                     blocked_1 = true;
                 }
             }
 
             if (!blocked_2 && hasValidBounds(_row, _col - i)) {
-                newRow = _row;
-                newCol = _col - i;
-
-                // checked tile is empty
-                if (gamestate.getPiece(newRow, newCol) == null) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                }
-
-                // checked tile has a capturable piece
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() != _playerId) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                    blocked_2 = true;
-                }
-
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() == _playerId) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                    blocked_2 = true;
-                }
-
-                // checked tile has one of own pieces
-                else {
+                validMoves.add(new ChessMove(null, _row, _col, _row, _col - i));
+                if (gamestate.getPiece(_row, _col - i) != null) {
                     blocked_2 = true;
                 }
             }
 
             if (!blocked_3 && hasValidBounds(_row + i, _col)) {
-                newRow = _row + i;
-                newCol = _col;
-
-                // checked tile is empty
-                if (gamestate.getPiece(newRow, newCol) == null) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                }
-
-                // checked tile has a capturable piece
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() != _playerId) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                    blocked_3 = true;
-                }
-
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() == _playerId) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                    blocked_3 = true;
-                }
-
-                // checked tile has one of own pieces
-                else {
+                validMoves.add(new ChessMove(null, _row, _col, _row + i, _col));
+                if (gamestate.getPiece(_row + i, _col) != null) {
                     blocked_3 = true;
                 }
             }
 
             if (!blocked_4 && hasValidBounds(_row - i, _col)) {
-                newRow = _row - i;
-                newCol = _col;
-
-                // checked tile is empty
-                if (gamestate.getPiece(newRow, newCol) == null) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                }
-
-                // checked tile has a capturable piece
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() != _playerId) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                    blocked_4 = true;
-                }
-
-                else if (gamestate.getPiece(newRow, newCol).getPlayerId() == _playerId) {
-                    validMoves.add(new ChessMove(_tempPlayer, _row, _col, newRow, newCol));
-                    blocked_4 = true;
-                }
-
-                // checked tile has one of own pieces
-                else {
+                validMoves.add(new ChessMove(null, _row, _col, _row - i, _col));
+                if (gamestate.getPiece(_row - i, _col) != null) {
                     blocked_4 = true;
                 }
             }
