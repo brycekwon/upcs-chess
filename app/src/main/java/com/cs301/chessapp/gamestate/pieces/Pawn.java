5e97aa042051202c2a4e6e73eecb830882f8db08
Bryce Kwon <kwon25@up.edu>
2023-04-28 16:59:10 -0700

    Add comments and minor fixes for Pawn.java
    - Updated documentation and comments
    - Added newPiece variable to lower calls to getPiece() method
    - Removed unnecessary checks for getChecks() method
    - Temporarily commented out initial 2-space starter move for the getChecks() method as it
    was causing issues for the King's check algorithm.

--- app/src/main/java/com/cs301/chessapp/gamestate/pieces/Pawn.java
+++ app/src/main/java/com/cs301/chessapp/gamestate/pieces/Pawn.java
@@ -3,16 +3,17 @@ package com.cs301.chessapp.gamestate.pieces;
 import java.util.ArrayList;
 
 import com.cs301.chessapp.gameframework.players.GamePlayer;
+
 import com.cs301.chessapp.gamestate.ChessGameState;
 import com.cs301.chessapp.gamestate.chessboard.ChessMove;
 
 /**
  * Pawn class
- *
- * This class represents a pawn piece in a game of chess. The pawn can move
+ * <p>
+ * This class represents a pawn in the game of chess. The pawn can move
  * one square forward. If it is in its starting position, it can move two
- * squares forward. It can capture an enemy piece on the square diagonally in
- * front of it. It is worth 1 point.
+ * squares forward. It captures pieces diagonally forward. It is a subclass
+ * of Piece.
  *
  * @author Bryce Kwon
  * @author Christopher Yee
@@ -23,53 +24,55 @@ import com.cs301.chessapp.gamestate.chessboard.ChessMove;
 public class Pawn extends Piece {
 
     /**
-     * Pawn constructor
+     * Pawn default constructor
+     * <p>
+     * This constructor initializes a pawn assigned to a player.
      *
-     * This constructor initializes a pawn with a player.
-     *
-     * @param player        the player the piece belongs to
+     * @param playerId      player assigned to this piece
      */
-    public Pawn(int player) {
-        super(player, "Pawn");
+    public Pawn(int playerId) {
+        // invoke superclass constructor
+        super(playerId, "Pawn");
     }
 
     /**
      * getMoves
+     * <p>
+     * This method calculates all valid moves for the pawn at its current
+     * position on the chessboard.
      *
-     * This method returns all valid moves for the pawn.
-     *
-     * @param gamestate     the current gamestate
-     * @param player        the player making the move
-     * @return              a list of valid moves
+     * @param gamestate     current state of the game
+     * @param player        player assigned to this piece
+     * @return              valid moves for the pawn
      */
     @Override
     public ArrayList<ChessMove> getMoves(ChessGameState gamestate, GamePlayer player) {
+        // initialize an empty list of valid moves
         ArrayList<ChessMove> validMoves = new ArrayList<>();
 
-        // check moves from the white player's perspective
+        // traverse through moves from the white player's perspective
         if (_playerId == ChessGameState.PLAYER_1) {
 
-            if (hasValidBounds(_row - 1, _col)) {
-
-                // checked tile is empty
-                if (gamestate.getPiece(_row - 1, _col) == null) {
-                    validMoves.add(new ChessMove(player, _row, _col, _row - 1, _col));
-                }
+            // current tile is empty
+            if (hasValidBounds(_row - 1, _col) && gamestate.getPiece(_row - 1, _col) == null) {
+                validMoves.add(new ChessMove(player, _row, _col, _row - 1, _col));
             }
 
             // can move 2 forward from starting position
-            if (_row == 6 && (gamestate.getPiece(_row - 2, _col) == null && gamestate.getPiece(_row - 1, _col) == null)) {
-                validMoves.add(new ChessMove(player, _row, _col, _row - 2, _col));
+            if (_row == 6) {
+                if (gamestate.getPiece(_row - 2, _col) == null && gamestate.getPiece(_row - 1, _col) == null) {
+                    validMoves.add(new ChessMove(player, _row, _col, _row - 2, _col));
+                }
             }
 
-            // checked tile has a capturable piece
+            // current tile has a capturable piece
             if (hasValidBounds(_row - 1, _col - 1) && gamestate.getPiece(_row - 1, _col - 1) != null) {
                 if (gamestate.getPiece(_row - 1, _col - 1).getPlayerId() != _playerId) {
                     validMoves.add(new ChessMove(player, _row, _col, _row - 1, _col - 1));
                 }
             }
 
-            // checked tile has a capturable piece
+            // current tile has a capturable piece
             if (hasValidBounds(_row - 1, _col + 1) && gamestate.getPiece(_row - 1, _col + 1) != null) {
                 if (gamestate.getPiece(_row - 1, _col + 1).getPlayerId() != _playerId) {
                     validMoves.add(new ChessMove(player, _row, _col, _row - 1, _col + 1));
@@ -77,30 +80,28 @@ public class Pawn extends Piece {
             }
         }
 
-        // check moves from the black player's perspective
+        // traverse through moves from the black player's perspective
         else if (_playerId == ChessGameState.PLAYER_2) {
 
-            if (hasValidBounds(_row + 1, _col)) {
-
-                // checked tile is empty
-                if (gamestate.getPiece(_row + 1, _col) == null) {
-                    validMoves.add(new ChessMove(player, _row, _col, _row + 1, _col));
-                }
+            if (hasValidBounds(_row + 1, _col) && gamestate.getPiece(_row + 1, _col) == null) {
+                validMoves.add(new ChessMove(player, _row, _col, _row + 1, _col));
             }
 
             // can move 2 forward from starting position
-            if (_row == 1 && (gamestate.getPiece(_row + 2, _col) == null && gamestate.getPiece(_row + 1, _col) == null)) {
-                validMoves.add(new ChessMove(player, _row, _col, _row + 2, _col));
+            if (_row == 1) {
+                if (gamestate.getPiece(_row + 2, _col) == null && gamestate.getPiece(_row + 1, _col) == null) {
+                    validMoves.add(new ChessMove(player, _row, _col, _row + 2, _col));
+                }
             }
 
-            // checked tile has a capturable piece
+            // current tile has a capturable piece
             if (hasValidBounds(_row + 1, _col - 1) && gamestate.getPiece(_row + 1, _col - 1) != null) {
                 if (gamestate.getPiece(_row + 1, _col - 1).getPlayerId() != _playerId) {
                     validMoves.add(new ChessMove(player, _row, _col, _row + 1, _col - 1));
                 }
             }
 
-            // checked tile has a capturable piece
+            // current tile has a capturable piece
             if (hasValidBounds(_row + 1, _col + 1) && gamestate.getPiece(_row + 1, _col + 1) != null) {
                 if (gamestate.getPiece(_row + 1, _col + 1).getPlayerId() != _playerId) {
                     validMoves.add(new ChessMove(player, _row, _col, _row + 1, _col + 1));
@@ -108,40 +109,76 @@ public class Pawn extends Piece {
             }
         }
 
+        // return the list of valid moves
         return validMoves;
     }
 
+    /**
+     * getChecks
+     * <p>
+     * This methods calculates all checkable moves for the pawn at its
+     * current position on the chessboard. This method is used to determine
+     * whether the pawn is putting the opposing player's king in check.
+     *
+     * @param gamestate     current state of the game
+     * @return              checkable moves for the pawn
+     */
+    @Override
     public ArrayList<ChessMove> getChecks(ChessGameState gamestate) {
         ArrayList<ChessMove> validMoves = new ArrayList<>();
 
         // check moves from the white player's perspective
         if (_playerId == ChessGameState.PLAYER_1) {
 
+//            // current tile is empty
+//            if (hasValidBounds(_row - 1, _col) && gamestate.getPiece(_row - 1, _col) == null) {
+//                validMoves.add(new ChessMove(null, _row, _col, _row - 1, _col));
+//            }
+//
+//            // can move 2 forward from starting position
+//            if (_row == 6) {
+//                if (gamestate.getPiece(_row - 2, _col) == null && gamestate.getPiece(_row - 1, _col) == null) {
+//                    validMoves.add(new ChessMove(null, _row, _col, _row - 2, _col));
+//                }
+//            }
+
             // checked tile has a capturable piece
             if (hasValidBounds(_row - 1, _col - 1)) {
-                validMoves.add(new ChessMove(_tempPlayer, _row, _col, _row - 1, _col - 1));
+                validMoves.add(new ChessMove(null, _row, _col, _row - 1, _col - 1));
             }
 
             // checked tile has a capturable piece
             if (hasValidBounds(_row - 1, _col + 1)) {
-                validMoves.add(new ChessMove(_tempPlayer, _row, _col, _row - 1, _col + 1));
+                validMoves.add(new ChessMove(null, _row, _col, _row - 1, _col + 1));
             }
         }
 
         // check moves from the black player's perspective
         else if (_playerId == ChessGameState.PLAYER_2) {
 
-            // checked tile has a capturable piece
+//            if (hasValidBounds(_row + 1, _col) && gamestate.getPiece(_row + 1, _col) == null) {
+//                validMoves.add(new ChessMove(null, _row, _col, _row + 1, _col));
+//            }
+//
+//            // can move 2 forward from starting position
+//            if (_row == 1) {
+//                if (gamestate.getPiece(_row + 2, _col) == null && gamestate.getPiece(_row + 1, _col) == null) {
+//                    validMoves.add(new ChessMove(null, _row, _col, _row + 2, _col));
+//                }
+//            }
+
+            // current tile has a capturable piece
             if (hasValidBounds(_row + 1, _col - 1)) {
-                validMoves.add(new ChessMove(_tempPlayer, _row, _col, _row + 1, _col - 1));
+                validMoves.add(new ChessMove(null, _row, _col, _row + 1, _col - 1));
             }
 
-            // checked tile has a capturable piece
+            // current tile has a capturable piece
             if (hasValidBounds(_row + 1, _col + 1)) {
-                validMoves.add(new ChessMove(_tempPlayer, _row, _col, _row + 1, _col + 1));
+                validMoves.add(new ChessMove(null, _row, _col, _row + 1, _col + 1));
             }
         }
 
+        // return the list of valid moves
         return validMoves;
     }
 }
